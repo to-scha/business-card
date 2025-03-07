9ec2a6e7aa28720e15325ec575631a33aeeeb91a
Tobias Schad-Hirschmüller <github@schad-info.de>
2025-03-07 17:14:43 +0100

    update project layout

--- src/app/components/projects-card/projects-card.component.ts
+++ src/app/components/projects-card/projects-card.component.ts
@@ -1,15 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {AsyncPipe, DatePipe, NgForOf} from '@angular/common';
+import {AsyncPipe, DatePipe, NgForOf, NgIf} from '@angular/common';
 import {Project} from './project.type';
 import {
   MatAccordion,
   MatExpansionPanel,
-  MatExpansionPanelDescription, MatExpansionPanelHeader,
+  MatExpansionPanelHeader,
   MatExpansionPanelTitle
 } from '@angular/material/expansion';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {MatChip, MatChipSet} from '@angular/material/chips';
 
 @Component({
   selector: 'app-projects-card',
@@ -22,17 +25,34 @@ import {
     MatExpansionPanel,
     MatExpansionPanelHeader,
     MatExpansionPanelTitle,
-    MatExpansionPanelDescription
+    MatIcon,
+    MatIconButton,
+    NgIf,
+    MatChip,
+    MatChipSet
   ],
   templateUrl: './projects-card.component.html',
   styleUrl: './projects-card.component.sass'
 })
 export class ProjectsCardComponent {
+  @ViewChild('projectAccordion') projectAccordion: MatAccordion | undefined;
+
+  public toggleView: boolean = true;
   public projects: Observable<Project[]>;
   private projectUrl = 'projects.json';
 
   constructor(private http: HttpClient) {
     this.projects = this.getProjects();
+  }
+
+  public openAllPanels() {
+    this.projectAccordion?.openAll();
+    this.toggleView = true;
+  }
+
+  public closeAllPanels() {
+    this.projectAccordion?.closeAll();
+    this.toggleView = false;
   }
 
   private getProjects(): Observable<Project[]> {
